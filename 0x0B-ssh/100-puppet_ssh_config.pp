7f79fad9f1963935bad72304ab52204521af785d
Adora <adoraonyile@gmail.com>
2024-05-09 19:01:23 +0100

    checker fail

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # Puppet script to create ssh config file
-file { 'etc/ssh/ssh_config':
-        ensure => present,
 
-content =>"
-
-        #SSH client configuration
-        host*
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-	",
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+content  => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
