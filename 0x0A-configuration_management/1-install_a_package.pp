0a79fd5e6cd9bdf0510dbdee7890898c6ecba596
Adora <adoraonyile@gmail.com>
2024-05-09 11:38:11 +0100

    checker fail

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Install Flask package from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+package { 'Flask' :
+          ensure   => '2.1.0',
+          provider => 'pip3'
+        }
+
+# Werkzeug
+package { 'Werkzeug':
+            ensure   => '2.2.1',
+            provider => 'pip3',
+          }
