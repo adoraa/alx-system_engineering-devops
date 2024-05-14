6c0e5e815371fb01527b41fed9ca325f86e0c9f6
Adora <adoraonyile@gmail.com>
2024-05-14 01:23:29 +0100

    puppet-lint correction

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,43 @@
+# Add a custom HTTP header with Puppet
 # File: 2-puppet_custom_http_response_header.pp
+
+# Update system
+exec { 'update system':
+    command => '/usr/bin/apt-get update',
+}
 
 # Install nginx package
 package { 'nginx':
-  ensure => installed,
+    ensure  => installed,
+    require => Exec['update system'],
+}
+
+# Define nginx configuration file
+file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
 }
 
 # Define nginx service
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
 }
 
-# Define custom HTTP header configuration file
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => present,
-  content => "add_header X-Served-By $hostname;",
-  notify  => Service['nginx'],
+# Define custom HTTP header configuration
+file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
+
+# Execute command to add custom HTTP header
+exec { 'add_custom_http_header':
+    command => '/bin/sed -i "24i\        add_header X-Served-By $(facter hostname);" /etc/nginx/sites-available/default',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
 }
