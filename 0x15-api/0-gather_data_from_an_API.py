3397d6b749c05ffe6221a5df04749f1a7295accd
Adora <adoraonyile@gmail.com>
2024-06-03 15:10:17 +0100

    Gather data from an API fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,38 +8,27 @@ import requests
 import sys
 
 
-def main(employee_id):
-    # Base URL of the API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Get employee details
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    # Get TODO list for the employee
-    todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todos_data = todos_response.json()
-
-    # Calculate tasks statistics
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task.get("completed")]
-    number_of_done_tasks = len(completed_tasks)
-
-    # Print the employee TODO list progress
-    print(f"Employee {employee_name}
-          is done with tasks({number_of_done_tasks}/{total_tasks}): ")
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+REST_API = "https://jsonplaceholder.typicode.com"
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        try:
+            id = int(sys.argv[1])
+            user_url = f"{REST_API}/users/{id}"
+            todos_url = f"{REST_API}/todos?userId={id}"
+
+            user_response = requests.get(user_url).json()
+            todos_response = requests.get(todos_url).json()
+
+            emp_name = user_response.get('name')
+            total_tasks = len(todos_response)
+            completed_tasks = [task for task in todos_response if task.get('completed')]
+
+            print(f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{total_tasks}):')
+            for task in completed_tasks:
+                print(f'\t {task.get("title")}')
+        except ValueError:
+            print("Employee ID must be an integer.")
+    else:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-    main(employee_id)
