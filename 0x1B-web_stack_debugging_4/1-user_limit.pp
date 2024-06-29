0e46afbeeb2959e0960f030f4aee0d67f3cac6d7
Adora <adoraonyile@gmail.com>
2024-06-29 18:59:14 +0100

    login and chage users without errors

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change the OS configuration to login with the
+# holberton user and open a file without errors.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
